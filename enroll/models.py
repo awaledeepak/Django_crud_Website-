4eda77d15eac2b57e18ab33b4093ffb611fd3626
Deepak <deepakvawale@gmail.com>
2022-05-13 16:24:23 +0530

    Initial commit

---
+++ enroll/models.py
@@ -0,0 +1,15 @@
+from django.db import models 
+
+
+# Create your models here.
+class User(models.Model): 
+    name = models.CharField(max_length=70) 
+    email = models.EmailField(max_length=100) 
+    phone = models.CharField(max_length=20, blank=True)  
+    address = models.CharField(max_length=70)
+    password = models.CharField(max_length=100)   
+    
+    
+     
+    
+    
